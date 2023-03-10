fe6cdc0cc763b167afb1626bdf5f9a9986b8eff6
Jonathan Staab <shtaab@gmail.com>
2023-03-10 09:53:05 -0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      black: "#0f0f0e",
+      white: "#FFFCF2",
+      accent: "#EB5E28",
+      accentl: "#EE7648",
+      light: "#CCC5B9",
+      shimmer: "#544e46",
+      medium: "#403D39",
+      dark: "#252422",
+      danger: "#ff0000",
+      warning: "#ebd112",
+      success: "#37ab51",
+      placeholder: "#a19989",
+    },
+  },
+  plugins: [],
+}
