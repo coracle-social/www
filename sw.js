779bec1539dc86a41dff9a9c53db61080c6bc350
Jonathan Staab <shtaab@gmail.com>
2023-03-10 10:49:46 -0600

    Add sw.js to replace the old one from the app

---
+++ sw.js
@@ -0,0 +1,14 @@
+// Listen for request events
+self.addEventListener('fetch', function (event) {
+
+	// Get the request
+	let request = event.request;
+
+	// Bug fix
+	// https://stackoverflow.com/a/49719964
+	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+
+	// HTML files
+	// Network-first
+	event.respondWith(fetch(request))
+});
