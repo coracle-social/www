956b75830144316984554afb0799a4e0e641023e
Jonathan Staab <shtaab@gmail.com>
2023-03-10 10:53:33 -0600

    Simplify sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-// Listen for request events
-self.addEventListener('fetch', function (event) {
-
-	// Get the request
-	let request = event.request;
-
-	// Bug fix
-	// https://stackoverflow.com/a/49719964
-	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
-
-	// HTML files
-	// Network-first
-	event.respondWith(fetch(request))
-});
+self.addEventListener('fetch', event => event.respondWith(fetch(event.request)))
